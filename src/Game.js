846a72400fbf97c3466337fd3389815e2d3217bc
jefree <jgarzon920429@gmail.com>
2015-07-11 13:45:54 -0500

    add player movement

--- src/Game.js
+++ src/Game.js
@@ -10,8 +10,26 @@ Pong.Game.prototype.preload =  function() {
 }
 
 Pong.Game.prototype.create = function() {
-  console.log('create')
+  console.log('create');
+
+  this.game.input.keyboard.addKeyCapture([
+    Phaser.Keyboard.UP,
+    Phaser.Keyboard.DOWN
+  ]);
 
   this.player = new Pong.Player(this.game, 0, 0, 'paddle');
 
+}
+
+Pong.Game.prototype.update = function() {
+  var isDown = this.input.keyboard.isDown;
+  var velocity = 300 * this.game.time.elapsed / 1000;
+
+  if (this.input.keyboard.isDown(Phaser.Keyboard.UP)){
+    this.player.y -= velocity;
+  }
+
+  if (this.input.keyboard.isDown(Phaser.Keyboard.DOWN)) {
+    this.player.y += velocity;
+  }
 }
